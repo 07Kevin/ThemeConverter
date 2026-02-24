9f1c1c1db137221ecbc010d391833b573d904d11
Per Ghosh <per@gorep.se>
2026-02-24 17:26:47 +0100

    chore

---
+++ TASKS.h
@@ -0,0 +1,43 @@
+/*
+
+   #  -- ThemeConverter --
+
+   @PROJECT [name: ThemeConverter] [tag: cli, converter, application] [description: command line application for converting between formats] 
+
+   @TASK [tag: application] [project: ThemeConverter]
+   [description: Create first main files and add main method to ThemeConverter] 
+
+   @TASK [tag: application] [project: ThemeConverter] [summary: Implement command line argument parsing]
+   [description: Implement command line argument parsing for ThemeConverter, allowing users to specify input and output formats, as well as file paths]
+
+   @TASK [tag: application] [project: ThemeConverter] [summary: Check input file format]
+   [description: Check the format of the input file to ensure it is supported by ThemeConverter and set the appropriate internal logic to read it]
+
+   @TASK [tag: application] [project: ThemeConverter] [summary: Read theme source file]
+   [description: Read the theme source file specified by the user, ensuring it is in a supported format]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
